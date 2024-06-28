ba49831520a2b84d530bf5c6d9976784ac0548b1
AntonChubarov <achlp600@gmail.com>
2024-06-27 20:46:29 -0400

    feat: done task 4

--- src/4.ts
+++ src/4.ts
@@ -1,7 +1,70 @@
+import { v4 as uuid } from 'uuid';
+
+class Key {
+    private signature: string;
+
+    constructor() {
+        this.signature = uuid();
+    }
+
+    public getSignature(): string {
+        return this.signature;
+    }
+}
+
+class Person {
+    public readonly name: string;
+    private key: Key;
+
+    constructor(name: string, key: Key) {
+        this.key = key;
+    }
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    protected constructor(key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`${person.name} has entered the house.`);
+        } else {
+            console.log(`${person.name} was trying to enter the house, but the door is closed`);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('the door is open');
+        } else {
+            console.log('the key does not match');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person('John', key);
 
 house.openDoor(person.getKey());
 
